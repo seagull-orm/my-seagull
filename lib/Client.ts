71157c2e99bc6619d0efe16b44d25f30c2a7d0a1
Marty42Martinez <manuel.marty.martinez@gmail.com>
2020-06-12 16:48:29 -0700

    nothing fishy

--- lib/Client.ts
+++ lib/Client.ts
@@ -1,5 +1,3 @@
 import { Client } from 'pg';
 
-export const client = new Client({
-    connectionString: 'postgres://fxrtybdrskoyqu:b77425e111726b4134913d6bff97a6f5b88e97e73bdbe52cb9d2e7d10e670e23@ec2-3-231-16-122.compute-1.amazonaws.com:5432/dac97o2f57eqg2'
-})
+export const client = new Client({})
