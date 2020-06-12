26554579f08cb095f4d9d3e43a441a141fd227c6
Jacob Harrington <jharrington4@gmail.com>
2020-06-12 11:44:14 -0700

    recommit

--- lib/Model.ts
+++ lib/Model.ts
@@ -2,7 +2,9 @@ import Schema from './Schema';
 import pluralize from 'pluralize';
 
 export default class Model {
+  tableName: string;
   constructor(name: string, schema: Schema){
-    this.tableName = pluralize(name).toLowerCase()
+    this.tableName = pluralize(name).toLowerCase();
+    
   }
 }
